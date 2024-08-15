6151d852a62405dde0dfa9f47a9ec5faabcfde00
Espe3ar <esperanzabardelli@gmail.com>
2024-08-14 21:09:42 -0300

    refactor: refactor create  method in OrderManager

--- app/repositories/managers.py
+++ app/repositories/managers.py
@@ -72,24 +72,20 @@ class OrderManager(BaseManager):
         cls.session.flush()
         cls.session.refresh(new_order)
         try:
-            for ingredient in ingredients:
-                cls.session.add(
-                    OrderIngredient(
-                        order_id=new_order._id,
-                        ingredient_id=ingredient._id,
-                        ingredient_price=ingredient.price,
-                    )
-                )
-
-            for beverage in beverages:
-                cls.session.add(
-                    OrderBeverage(
-                        order_id=new_order._id,
-                        beverage_id=beverage._id,
-                        beverage_price=beverage.price,
-                    )
-                )
-
+            cls.session.add_all((
+            OrderIngredient(
+                order_id=new_order._id,
+                ingredient_id=ingredient._id,
+                ingredient_price=ingredient.price,
+            ) for ingredient in ingredients
+        ))
+            cls.session.add_all((
+            OrderBeverage(
+                order_id=new_order._id,
+                beverage_id=beverage._id,
+                beverage_price=beverage.price,
+            ) for beverage in beverages
+        ))
             cls.session.refresh(new_order)
             cls.session.commit()
             return cls.serializer().dump(new_order)
